95c5a924007fcf9ef2835542a800a2380a2f4528
Siva Ganesh <sivaganeshv1729@gmail.com>
2026-01-13 21:05:32 +0530

    chore(widget): setup project configuration and build scripts

---
+++ checkout-widget/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/sdk/PaymentGateway.js',
+    output: {
+        filename: 'checkout.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: {
+            name: 'PaymentGateway',
+            type: 'umd',
+            export: 'default'
+        },
+        globalObject: 'this'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+};
